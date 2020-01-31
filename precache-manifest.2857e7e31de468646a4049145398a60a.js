85289c72faa5a12d3e3e8c5473199f4629077be9
erfun-tavak <erfuntavakoli1396@gmail.com>
2020-01-31 10:27:15 -0600

    Updates

---
+++ precache-manifest.2857e7e31de468646a4049145398a60a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607ed43380b0f2114d2de315595c1ada",
+    "url": "/facepook/index.html"
+  },
+  {
+    "revision": "25666fefcb04d6018e6e",
+    "url": "/facepook/static/css/main.9f551171.chunk.css"
+  },
+  {
+    "revision": "dda25f70cb2ba54713ec",
+    "url": "/facepook/static/js/2.9241846a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/facepook/static/js/2.9241846a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25666fefcb04d6018e6e",
+    "url": "/facepook/static/js/main.755a452e.chunk.js"
+  },
+  {
+    "revision": "881542131618c4355bf5",
+    "url": "/facepook/static/js/runtime-main.b7d3707e.js"
+  }
+]);
